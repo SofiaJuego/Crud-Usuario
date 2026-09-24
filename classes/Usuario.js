275fc065bf5040fcc49c71b60850ece0727b77b7
SofiaJuego <sofia.aguilar.cv@gmail.com>
2026-09-24 00:55:26 -0300

    ultomos cambios

--- classes/Usuario.js
+++ classes/Usuario.js
@@ -1,63 +1,44 @@
-// Extiende la clase Usuario para que contenga las siguiente propiedades:
-
-// Públicas: id, nombre, edad, email, teléfono, activo
-// Privadas: password
-
-// Para la carga de los datos utilizaremos un formulario que contega todas las propiedades 
-// (el usuario debe escribir 2 veces la password), 
-// junto con un botón "Guardar", el cuál disparará la obtención de los datos. 
-// Finalmente, se debe agegar el objeto del usuario cargado a un Array que comenzará vacío.
-
-// Utilizar CSS para estilos, preferentemente TailwindCSS. Realizar validaciones.
-
-// A. Clase Usuario (classes/Usuario.js)
-
-// La clase debe estar modularizada (utilizando export / import de ES6) y contar
-// con las siguientes propiedades:
-
-//   - Propiedades Públicas:
-//       - id (puede ser autoincremental o generado dinámicamente)
-//       - nombre (string)
-//       - edad (number)
-//       - email (string)
-//       - telefono (string)
-//       - activo (boolean)
-//   - Propiedad Privada:
-//       - password (string)
 class Usuario {
-    id
-    nombre
-    edad
-    email
-    telefono
-    activo
-    #password
+  id;
+  nombre;
+  edad;
+  email;
+  telefono;
+  activo;
+  #password;
 
-    constructor(id,nombre, edad,email, telefono, activo, password) {
-        if (typeof email !== "string" || !email.includes("@") || !email.includes(".")) {
-            throw new Error("El email ingresado no es válido")
-        }
-        if (typeof password !== "string" || password.length < 8) {
-            throw new Error("La contraseña debe tener al menos 8 caracteres")
-        }
-
-        if(typeof edad !== "number" || edad <= 0 || edad > 99) {
-            throw new Error("La edad ingresada no es válida, debe ser un número entre 1 y 99")
-        }
-
-        if(typeof telefono !== "string" || telefono.length < 8) {
-            throw new Error("El teléfono ingresado no es válido, debe tener al menos 8 caracteres")
-        }
-        
-        this.id = id
-        this.nombre = nombre
-        this.edad = edad
-        this.email = email
-        this.telefono = telefono
-        this.activo = activo
-        this.#password = password
+  constructor(id, nombre, edad, email, telefono, activo, password) {
+    if (
+      typeof email !== "string" ||
+      !email.includes("@") ||
+      !email.includes(".")
+    ) {
+      throw new Error("El email ingresado no es válido");
+    }
+    if (typeof password !== "string" || password.length < 8) {
+      throw new Error("La contraseña debe tener al menos 8 caracteres");
     }
 
+    if (typeof edad !== "number" || edad <= 0 || edad > 99) {
+      throw new Error(
+        "La edad ingresada no es válida, debe ser un número entre 1 y 99",
+      );
+    }
+
+    if (typeof telefono !== "string" || telefono.length < 8) {
+      throw new Error(
+        "El teléfono ingresado no es válido, debe tener al menos 8 caracteres",
+      );
+    }
+
+    this.id = id;
+    this.nombre = nombre;
+    this.edad = edad;
+    this.email = email;
+    this.telefono = telefono;
+    this.activo = activo;
+    this.#password = password;
+  }
 }
 
 export default Usuario;
--- index.js
+++ index.js
@@ -1,104 +1,96 @@
-// Extiende la clase Usuario para que contenga las siguiente propiedades:
-// Públicas: id, nombre, edad, email, teléfono, activo
-// Privadas: password
-// Para la carga de los datos utilizaremos un formulario que contega todas las propiedades 
-// (el usuario debe escribir 2 veces la password), 
-// junto con un botón "Guardar", el cuál disparará la obtención de los datos. 
-// Finalmente, se debe agegar el objeto del usuario cargado a un Array que comenzará vacío.
+import Usuario from "./classes/Usuario.js";
 
-// Utilizar CSS para estilos, preferentemente TailwindCSS. Realizar validaciones.
-
-import Usuario from "./classes/Usuario.js"
-
-const formulario = document.getElementById("formulario")
-const btnGuardar = document.getElementById("btnGuardar")
-const password = document.getElementById("password")
-const password2 = document.getElementById("password2")
-const nombre = document.getElementById("nombre")
-const edad = document.getElementById("edad")
-const email = document.getElementById("email")
-const telefono = document.getElementById("telefono")
-const activo = document.getElementById("activo")
-const error = document.getElementById("error")
-const usuariosList = document.getElementById("usuarios")
-const listaUsuarios = []
-
+const formulario = document.getElementById("formulario");
+const btnGuardar = document.getElementById("btnGuardar");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+const nombre = document.getElementById("nombre");
+const edad = document.getElementById("edad");
+const email = document.getElementById("email");
+const telefono = document.getElementById("telefono");
+const activo = document.getElementById("activo");
+const error = document.getElementById("error");
+const usuariosList = document.getElementById("usuarios");
+const listaUsuarios = [];
 
 formulario.addEventListener("submit", (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    const valorNombre = nombre.value.trim()
-    const valorEdad = parseInt(edad.value.trim())
-    const valorEmail = email.value.trim()
-    const valorTelefono = telefono.value.trim()
-    const valorActivo = activo.checked
-    const valorPassword = password.value.trim()
-    const valorPassword2 = password2.value.trim()
+  const valorNombre = nombre.value.trim();
+  const valorEdad = parseInt(edad.value.trim());
+  const valorEmail = email.value.trim();
+  const valorTelefono = telefono.value.trim();
+  const valorActivo = activo.checked;
+  const valorPassword = password.value.trim();
+  const valorPassword2 = password2.value.trim();
 
-    registrarUsuario({ nombre: valorNombre, 
-            edad: valorEdad, 
-            email: valorEmail, 
-            telefono: valorTelefono, 
-            activo: valorActivo, 
-            password: valorPassword, 
-            password2: valorPassword2 })
-
-
-})
+  registrarUsuario({
+    nombre: valorNombre,
+    edad: valorEdad,
+    email: valorEmail,
+    telefono: valorTelefono,
+    activo: valorActivo,
+    password: valorPassword,
+    password2: valorPassword2,
+  });
+});
 
 function renderUsuarios() {
-    usuariosList.innerHTML = ""
-    listaUsuarios.forEach(usuario => {
-        const tr = document.createElement("tr")
-        tr.className = "border-b text-sm text-gray-700"
-        tr.innerHTML = `
+  usuariosList.innerHTML = "";
+  listaUsuarios.forEach((usuario) => {
+    const tr = document.createElement("tr");
+    tr.className = "border-b text-sm text-gray-700";
+    tr.innerHTML = `
             <td class="p-2 text-center font-medium">${usuario.id}</td>
             <td class="p-2 whitespace-nowrap">${usuario.nombre}</td>
             <td class="p-2 text-center">${usuario.edad}</td>
             <td class="p-2 whitespace-nowrap">${usuario.telefono}</td>
             <td class="p-2 break-all">${usuario.email}</td>
             <td class="p-2 text-center">${usuario.activo ? "Sí" : "No"}</td>
-        `
-        usuariosList.appendChild(tr)
-    })
+        `;
+    usuariosList.appendChild(tr);
+  });
 }
 
 function registrarUsuario(datos) {
-    try {
-        if (datos.nombre === "" || isNaN(datos.edad) || datos.email === "" || datos.telefono === "" || datos.password === "" || datos.password2 === "") {
-            throw new Error("Todos los campos son obligatorios")
+  try {
+    if (
+      datos.nombre === "" ||
+      isNaN(datos.edad) ||
+      datos.email === "" ||
+      datos.telefono === "" ||
+      datos.password === "" ||
+      datos.password2 === ""
+    ) {
+      throw new Error("Todos los campos son obligatorios");
     }
 
-        if (datos.password !== datos.password2) {
-            throw new Error("Las contraseñas no coinciden")
-        }
-
-        if (listaUsuarios.some(usuario => usuario.email === datos.email)) {
-            throw new Error("El email ya está registrado")
-        }
-
-        const idGenerado = Math.floor(Math.random() * 1000)
-        const usuario = new Usuario(
-            idGenerado,
-            datos.nombre,
-            datos.edad,
-            datos.email,
-            datos.telefono,
-            datos.activo,
-            datos.password)
-        listaUsuarios.push(usuario)
-        renderUsuarios()
-
-        formulario.reset()
-        error.textContent = ""
-        error.classList.add("hidden")
-
-    } catch (err) {
-        error.textContent = err.message
-        error.classList.remove("hidden")
-
+    if (datos.password !== datos.password2) {
+      throw new Error("Las contraseñas no coinciden");
     }
+
+    if (listaUsuarios.some((usuario) => usuario.email === datos.email)) {
+      throw new Error("El email ya está registrado");
+    }
+
+    const idGenerado = Math.floor(Math.random() * 1000);
+    const usuario = new Usuario(
+      idGenerado,
+      datos.nombre,
+      datos.edad,
+      datos.email,
+      datos.telefono,
+      datos.activo,
+      datos.password,
+    );
+    listaUsuarios.push(usuario);
+    renderUsuarios();
+
+    formulario.reset();
+    error.textContent = "";
+    error.classList.add("hidden");
+  } catch (err) {
+    error.textContent = err.message;
+    error.classList.remove("hidden");
+  }
 }
-
-
-
